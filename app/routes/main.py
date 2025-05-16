73d043c1278d87baff1d4cac2e7ec56ab46cb430
DiegoVillarroelSandy <Diego.Villarroel@jala.university>
2025-05-16 07:35:24 -0400

    Implementando logger con singleton

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template
+from app.services.logger_service import Logger
 
 main_bp = Blueprint('main', __name__)
-
+log = Logger().get_logger()
 @main_bp.route('/')
 def index():
+    log.info("Ruta '/' accedida  correctamente")
     return render_template('index.html')
