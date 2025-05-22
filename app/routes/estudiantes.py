372a6d82462d0052051acd213d4f6e6a1be8092e
DiegoVillarroelSandy <Diego.Villarroel@jala.university>
2025-05-22 08:36:46 -0400

    ruta añadida para notas estudiante

---
+++ app/routes/estudiantes.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask_login import login_required, current_user
+from app.decorators import student_required
+from app.models import Estudiante, Ejercicio, RespuestaEstudiante, Contenido
+
+estudiantes_bp = Blueprint('estudiantes', __name__, url_prefix='/estudiantes')
+
+@estudiantes_bp.route('/')
+@login_required
+@student_required
+def index():
+    return render_template('estudiante/index.html')
+
+@estudiantes_bp.route('/notas')
+@login_required
+@student_required
+def notas():
+    
+    return render_template('estudiante/notas/index.html')
+
