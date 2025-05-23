e1b9fa17a96bd11e71e9d22a4d2277ab83e1a1dd
DiegoVillarroelSandy <Diego.Villarroel@jala.university>
2025-05-23 08:28:33 -0400

    quitando password para base de datos

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
 
 # Configuración de la base de datos PostgreSQL
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:playstation4@localhost/colegio'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:@localhost/colegio'
 #SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:p%2E210404@localhost/colegio'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = os.urandom(24)
